9a99d6cf2cc92e43161782651f4fb40cdd79a1aa
Siwakorn <sackae1901@gmail.com>
2016-11-05 11:38:21 +0700

    fitsh commit

---
+++ app/src/main/java/com/example/sackz/git/lowlistActivity.java
@@ -0,0 +1,13 @@
+package com.example.sackz.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lowlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lowlist);
+    }
+}
